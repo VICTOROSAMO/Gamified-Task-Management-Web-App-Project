aeaab7b744454159bc98decaaab95872943dd5d5
Osamo <osamovicky2021@gmail.com>
2025-02-16 18:30:25 +0300

    modified files

--- Gamified-Task-Management-Web-App Backend/src/main/java/com/Gamified/Task/Management/Web/App/GamifiedTaskManagementWebAppApplication.java
+++ Gamified-Task-Management-Web-App Backend/src/main/java/com/Gamified/Task/Management/Web/App/GamifiedTaskManagementWebAppApplication.java
@@ -2,8 +2,9 @@ package com.Gamified.Task.Management.Web.App;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class GamifiedTaskManagementWebAppApplication {
 
 	public static void main(String[] args) {
--- Gamified-Task-Management-Web-App Backend/src/main/java/com/Gamified/Task/Management/Web/App/controller/AuthController.java
+++ Gamified-Task-Management-Web-App Backend/src/main/java/com/Gamified/Task/Management/Web/App/controller/AuthController.java
@@ -1,15 +1,14 @@
 package com.Gamified.Task.Management.Web.App.controller;
 
 import com.Gamified.Task.Management.Web.App.entity.User;
+import com.Gamified.Task.Management.Web.App.respository.UserRepository;
 import com.Gamified.Task.Management.Web.App.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 @Component
 @RestController
@@ -18,6 +17,8 @@ import java.util.Map;
 public class AuthController {
         @Autowired
         private AuthService authService;
+        @Autowired
+        private UserRepository userRepository;
 
         @PostMapping("/register")
         public ResponseEntity<String> register(@RequestBody User user) {
@@ -28,4 +29,10 @@ public class AuthController {
         public ResponseEntity<String> login(@RequestBody Map<String, String> request) {
             return ResponseEntity.ok(authService.login(request.get("username"), request.get("password")));
         }
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
 }
